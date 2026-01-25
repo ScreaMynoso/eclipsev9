3dc9cd96c442133f7f835de058f31b55add3a4bc
ScreaMynoso <screamdelfino04@gmail.com>
2026-01-25 07:55:21 -0300

    Update AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { BottomNav } from "./BottomNav";
+import { DesktopSidebar } from "./DesktopSidebar";
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -8,10 +9,20 @@ interface AppLayoutProps {
 export const AppLayout = ({ children }: AppLayoutProps) => {
   return (
     <div className="min-h-screen bg-background stars-bg">
-      <main className="pb-24 pt-safe">
-        {children}
+      {/* Desktop Sidebar - hidden on mobile/tablet */}
+      <DesktopSidebar />
+      
+      {/* Main content - offset on desktop for sidebar */}
+      <main className="pb-24 pt-safe lg:pb-8 lg:pl-64 lg:pt-8">
+        <div className="lg:max-w-4xl lg:mx-auto lg:px-8">
+          {children}
+        </div>
       </main>
-      <BottomNav />
+      
+      {/* Bottom nav - hidden on desktop */}
+      <div className="lg:hidden">
+        <BottomNav />
+      </div>
     </div>
   );
 };
